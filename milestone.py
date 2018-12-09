180cc7d166cf6522087a23d2599ee14bf3248548
Nico Espinosa Dice <nespinosadice@hmc.edu>
2018-12-08 19:16:04 -0800

    Deleted old comments

--- milestone.py
+++ milestone.py
@@ -1,82 +1,105 @@
 GlowScript 2.7 VPython
-# Start.py
+# Milestone.py
 # Jacob van der Leeuw and Nico Espinosa Dice
 # Final Project - VPython
 
+gameOver = False
+
+# +++ Start of SCENE SETUP +++ 
 scene.bind('keydown', keydown_fun)     # Function for key presses
 scene.bind('click', click_fun)         # Function for mouse clicks
 scene.background = 0.8*vector(1, 1, 1) # Light gray (0.8 out of 1.0)
 scene.width = 640                      # Make the 3D canvas larger
 scene.height = 480
+# +++ End of SCENE SETUP +++ 
 
-# +++ start of TRACK CREATION +++
+# +++ Start of TRACK CREATION +++
 # Ground (represented by a box - vpython's rectangular solid)
 ground = box(size = vector(20, 1, 20), pos = vector(0, -1, 0), color = .4*vector(1, 1, 1))
 ground2 = box(size = vector(20, 1, 4), pos = vector(0, -1, 12), color = color.purple)
 
-# Outer-most boundaries of the track
-O_wallN = box(pos = vector(0, 0, -10), axis = vector(1, 0, 0), size = vector(20, 1, .2), color = color.red) # "North" wall - red
+# Outer Walls
+O_wallN = box(pos = vector(0, 0, -10), axis = vector(1, 0, 0), size = vector(20, 1, .2), color = color.blue) # "North" wall - red
 O_wallW = box(pos = vector(-10, 0, 0), axis = vector(0, 0, 1), size = vector(20, 1, .2), color = color.blue) # "West" wall - blue
-O_wallS = box(pos = vector(0, 0, 10), axis = vector(1, 0, 0), size = vector(16, 1, .2), color = color.green) # "South" wall - green
-O_wallE = box(pos = vector(10, 0, 0), axis = vector(0, 0, 1), size = vector(20, 1, .2), color = color.yellow) # "East" wall - yellow
+O_wallS = box(pos = vector(0, 0, 10), axis = vector(1, 0, 0), size = vector(16, 1, .2), color = color.yellow) # "South" wall - green
+O_wallE = box(pos = vector(10, 0, 0), axis = vector(0, 0, 1), size = vector(20, 1, .2), color = color.blue) # "East" wall - yellow
 
-# Inner walls of the track
-#east walls
-I_wallE = box(pos = vector(8, 0, 1), axis = vector(0, 0, 1), size = vector(18, 1, .2), color = color.black) # "East" wall - yellow
-I_wallE2 = box(pos = vector(4, 0, -1), axis = vector(0, 0, 1), size = vector(18, 1, .2), color = color.yellow) # "East" wall - yellow
+# Inner Walls
+# east walls
+I_wallE = box(pos = vector(8, 0, 1), axis = vector(0, 0, 1), size = vector(18, 1, .2), color = color.red) # "East" wall - yellow
+I_wallE2 = box(pos = vector(4, 0, -1), axis = vector(0, 0, 1), size = vector(18, 1, .2), color = color.green) # "East" wall - yellow
 
-#center wall
-I_wallC = box(pos = vector(0, 0, 1), axis = vector(0, 0, 1), size = vector(18, 1, .2), color = color.green) # "East" wall - yellow
+# center wall
+I_wallC = box(pos = vector(0, 0, 1), axis = vector(0, 0, 1), size = vector(18, 1, .2), color = color.white) # "East" wall - yellow
 
-#west walls
-I_wallW = box(pos = vector(-8, 0, 1), axis = vector(0, 0, 1), size = vector(18, 1, .2), color = color.white) # "East" wall - yellow
-I_wallW2 = box(pos = vector(-4, 0, -1), axis = vector(0, 0, 1), size = vector(18, 1, .2), color = color.red) # "East" wall - yellow
+# west walls
+I_wallW = box(pos = vector(-8, 0, 1), axis = vector(0, 0, 1), size = vector(18, 1, .2), color = color.red) # "East" wall - yellow
+I_wallW2 = box(pos = vector(-4, 0, -1), axis = vector(0, 0, 1), size = vector(18, 1, .2), color = color.green) # "East" wall - yellow
+# +++ End of TRACK CREATION +++ 
 
+
+# +++ Start of OBJECT CREATION +++ 
 # Ball (user controls)
 ball = sphere(pos = vector(9, 0, 9), size = 1.0*vector(1, 1, 1), color = vector(0.8, 0.5, 0.0))   # ball is an object of class sphere
 ball.vel = vector(0, 0, 0)     # this is its initial velocity
 
-# Cylinder (other control)
+# Chase Object #1 (autonomous)
 chaseObject1 = cylinder(pos = vector(8.5, 0, -5.5), size = 1.0*vector(1,1,1), color = color.orange)
 chaseObject1.vel = vector(0,0,-5) #initial velocity for chaseObj
 
-# Obstacle 1 (Moving Object)
+# Obstacle 1 (moving object)
 obstacle1 = sphere(pos = vector(-4, 0, 8.5), size = 0.5*vector(1, 1, 1), color = vector(1, 1, 1))   # ball is an object of class sphere
 obstacle1.vel = vector(-5, 0, 0)     # this is its initial velocity
-# +++ End of TRACK CREATION +++
-
 obstacle2 = sphere(pos = vector(4, 0, 8.5), size = 0.3*vector(1, 1, 1), color = vector(1, 1, 1))   # ball is an object of class sphere
 obstacle2.vel = vector(10, 0, 0)
+# +++ End of OBJECT CREATION ++
 
-# +++ start of ANIMATION section ++
 
-# Other constants
+# +++ Start of ANIMATION +++
+# Constants
 RATE = 30                # The number of times the while loop runs each second
 dt = 1.0/(1.0*RATE)      # The time step each time through the while loop
 scene.autoscale = False  # Avoids changing the view automatically
 scene.forward = vector(0, -3, -2)  # Ask for a bird's-eye view of the scene...
-# This is the "event loop" or "animation loop"
-# Each pass through the loop will animate one step in time, dt
+# +++ End of ANIMATION +++
 
-gameOver = False
+# +++ Start of GAME EXPLANATION to user +++ 
+print("Hurry, the runaway cylinder is getting away!")
+print("Use the arrow keys to move the ball through the track while avoiding the obstacles!")
+print("Tip: use the space bar to slow down before turns.")
+print("GOOD LUCK!!!")
+# +++ End of GAME EXPLANATION to user +++ 
 
-while not gameOver:    
+
+# +++ Start of GAME LOOP +++ 
+gameOver = False # Keeps track of whether the game is over
+
+while not gameOver: # Each pass through the loop will animate one step in time (dt) 
     rate(RATE)   # maximum number of times per second the while loop runs
 
-    # +++ Start of PHYSICS UPDATES -- update all positions here, every time step
-    ball.pos = ball.pos + ball.vel*dt      # Update the ball's position
-    # +++ End of PHYSICS UPDATES -- be sure new objects are updated appropriately!
-    chaseObject1.pos = chaseObject1.pos + chaseObject1.vel*dt
-    obstacle1.pos = obstacle1.pos + obstacle1.vel*dt
-    obstacle2.pos = obstacle2.pos + obstacle2.vel*dt
+    # +++ Start of PHYSICS UPDATES +++
+    # Update all positions here (every time step)
+    ball.pos = ball.pos + ball.vel*dt # Update the ball's position
+    chaseObject1.pos = chaseObject1.pos + chaseObject1.vel*dt # Update chaseObject1's position
+    obstacle1.pos = obstacle1.pos + obstacle1.vel*dt # Update obstacle1's position
+    obstacle2.pos = obstacle2.pos + obstacle2.vel*dt # Update obstacle2's position
+    # +++ End of PHYSICS UPDATES +++
+    
+    # +++ Start of EVENT CHECKING +++ 
+    # Ball
+    corral_collide(ball) # Checks to see if ball has collided with something
 
-    chaseObject_Path(chaseObject1)
-    corral_collide(ball)
-    obstacle1Collide(obstacle1)
-    obstacle2Collide(obstacle2)
+    # Chase Object
+    chaseObject_Path(chaseObject1) # Checks to see if chaseObject has collided with something (keeps chaseObject on correct path)
 
-# +++ start of EVENT_HANDLING section -- separate functions for
-#                                keypresses and mouse clicks...
+    # Obstacles
+    obstacle1Collide(obstacle1) # Checks to see if obstacle1 has collided with something
+    obstacle2Collide(obstacle2) # Checks to see if obstacle2 has collided with something
+     # +++ End of EVENT CHECKING +++ 
+
+
+# +++ start of EVENT_HANDLING section +++
+# Separate functions for keypresses and mouse clicks
 
 def gameFin(ball, chaseObject1):
     if (abs(ball.pos.x - chaseObject1.pos.x) < 0.10) and (abs(ball.pos.z - chaseObject1.pos.z) < 0.10):
@@ -176,6 +199,9 @@ def chaseObject_Path(chaseObject1):
 def corral_collide(ball):
     """Corral collisions!
     Ball must have a .vel field and a .pos field. """
+
+    global gameOver 
+
     # -- Outer walls --
     # If the ball hits O_wallE
     if (abs(ball.pos.x - O_wallE.pos.x) < 0.25) and (ball.pos.z >= -10) and (ball.pos.z <= 10):  # Hit -- check for z
